48dac0e97b20d989b0da41a807321cd33213e239
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-05-24 19:04:09 -0500

    messing around with base styles

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,38 @@
-<script>
+<script lang="ts">
+	import { Router, Route } from "svelte-navigator";
 	import Header from "./components/header.svelte";
+
 </script>
 
-<Header />
+
+<div class="app">
+	<Router>
+		<Header />
+
+		<main>
+			<Route path="/">
+				<h3>Home</h3>
+				<p>Home sweet home...</p>
+			</Route>
+		</main>
+
+	</Router>
+</div>
 
 <style>
+	:global(:root) {
+		--base-gray: #333;
+		--p1: #080705;
+		--p2: #141414;
+		--p3: #1a1a1c;
+		--p4: #1f2023;
+		--p5: #363841;
+		--p6: #393c45;
+		--p7: #40434e;
+		--p8: #51545e;
+		--p9: #61646d;
+		--txt: #FFF;
+		--link: #3e92cc;
+	}
+
 </style>
--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,6 +1,53 @@
-<script>
+<script lang="ts">
+	import {Link, link} from "svelte-navigator";
 </script>
 
+
 <header>
-	<h1>Chaz Kiker</h1>
+	<div class="wrapper">
+		<a href="/" use:link>
+			<h1>Chaz Kiker</h1>
+		</a>
+	</div>
+
+	<nav class="link-wrapper">
+		<Link class="custom-link" to="about">About</Link>
+		<Link class="custom-link" to="projects">Projects</Link>
+		<Link class="custom-link" to="contact">Contact</Link>
+	</nav>
 </header>
+
+
+
+<style>
+	header {
+		background-color: var(--p1);
+		padding: 0 2rem;
+		display: flex;
+		flex-flow: row nowrap;
+		justify-content: space-between;
+		align-items: center;
+	}
+	
+	h1 {
+		font-size: 3rem;
+		font-weight: 600;
+	}
+
+	.wrapper a {
+		text-decoration: none;
+	}
+
+	nav {
+		display: flex;
+		flex-flow: row nowrap;
+
+	}
+	.link-wrapper :global(.custom-link) {
+		display: inline-block;
+		background-color: var(--p2);
+		text-decoration: none;
+		color: var(--link);
+	}
+
+</style>
